1ca27a92bc07ceea23435da161ffa0018d9cbe5d
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-06-20 13:49:01 +0700

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -19,152 +19,219 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    String date = DateTime.now().toString();
+    DateTime now = new DateTime.now();
     return Scaffold(
-      body: SafeArea(
-        child: FutureBuilder<Covid>(
-          future: futureCovid,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Container(
-                margin: EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Covid-19',
-                      style: GoogleFonts.inter(
-                        fontSize: 32,
-                        fontWeight: FontWeight.w700,
+      backgroundColor: Color(0xFF1D1D1D),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        brightness: Brightness.dark,
+        title: Text(
+          'Covid-19',
+          style: GoogleFonts.inter(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Covid>(
+        future: futureCovid,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Container(
+              margin: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '🇮🇩 | Indonesia',
+                    style: GoogleFonts.inter(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        '${now.day}/${now.month}/${now.year} | ${now.hour}:${now.minute}:${now.second} WIB',
+                        style: GoogleFonts.inter(
+                          color: Colors.white60,
+                        ),
                       ),
-                    ),
-                    Text(
-                      date,
-                      style: GoogleFonts.inter(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.grey,
+                      SizedBox(
+                        width: 8,
                       ),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            DateTime.now();
+                          });
+                        },
+                        child: CircleAvatar(
+                          maxRadius: 10,
+                          child: Icon(
+                            Icons.restart_alt_rounded,
+                            color: Colors.white,
+                            size: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    height: 150,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16.0),
+                      color: Colors.yellow,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 24,
+                          color: Colors.black.withOpacity(0.05),
+                          spreadRadius: 8,
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      height: 32,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Container(
-                          padding: EdgeInsets.all(16.0),
-                          height: 120,
-                          width: 113,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16.0),
-                            color: Colors.yellow,
-                            boxShadow: [
-                              BoxShadow(
-                                blurRadius: 24,
-                                color: Colors.black.withOpacity(0.05),
-                                spreadRadius: 8,
-                              ),
-                            ],
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text(
-                                snapshot.data!.positif,
-                                style: GoogleFonts.inter(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                              Text(
-                                'Affected',
-                                style: GoogleFonts.inter(
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ],
+                        Text(
+                          snapshot.data!.positif,
+                          style: GoogleFonts.inter(
+                            fontSize: 36,
+                            fontWeight: FontWeight.w700,
                           ),
                         ),
-                        Container(
-                          padding: EdgeInsets.all(16.0),
-                          height: 120,
-                          width: 113,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16.0),
-                            color: Colors.green,
-                            boxShadow: [
-                              BoxShadow(
-                                blurRadius: 24,
-                                color: Colors.black.withOpacity(0.05),
-                                spreadRadius: 8,
-                              ),
-                            ],
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text(
-                                snapshot.data!.sembuh,
-                                style: GoogleFonts.inter(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                              Text(
-                                'Recovered',
-                                style: GoogleFonts.inter(
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.all(16.0),
-                          height: 120,
-                          width: 113,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16.0),
-                            color: Colors.red,
-                            boxShadow: [
-                              BoxShadow(
-                                blurRadius: 24,
-                                color: Colors.black.withOpacity(0.05),
-                                spreadRadius: 8,
-                              ),
-                            ],
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text(
-                                snapshot.data!.meninggal,
-                                style: GoogleFonts.inter(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                              Text(
-                                'Death',
-                                style: GoogleFonts.inter(
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ],
+                        Text(
+                          'Affected',
+                          style: GoogleFonts.inter(
+                            color: Colors.black45,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
                           ),
                         ),
                       ],
                     ),
-                  ],
-                ),
-              );
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
-            return CircularProgressIndicator();
-          },
-        ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    height: 150,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16.0),
+                      color: Colors.green.shade700,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 24,
+                          color: Colors.black.withOpacity(0.05),
+                          spreadRadius: 8,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          snapshot.data!.sembuh,
+                          style: GoogleFonts.inter(
+                            fontSize: 36,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          'Recovered',
+                          style: GoogleFonts.inter(
+                            color: Colors.black45,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    height: 150,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16.0),
+                      color: Colors.red.shade600,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 24,
+                          color: Colors.black.withOpacity(0.05),
+                          spreadRadius: 8,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          snapshot.data!.meninggal,
+                          style: GoogleFonts.inter(
+                            fontSize: 36,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          'Death',
+                          style: GoogleFonts.inter(
+                            color: Colors.black45,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'API from ',
+                        style: GoogleFonts.inter(
+                          color: Colors.white60,
+                          fontSize: 12,
+                        ),
+                      ),
+                      Text(
+                        'https://kawalcorona.com/api/',
+                        style: GoogleFonts.inter(
+                          color: Colors.white60,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          return CircularProgressIndicator();
+        },
       ),
     );
   }
